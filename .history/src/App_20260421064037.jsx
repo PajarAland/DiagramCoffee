c31659c957cec8b54c20d155b11fc8b812f68326
PajarAland <162042636+PajarAland@users.noreply.github.com>
2026-04-26 03:24:29 +0700

    Delete .history directory

--- .history/src/App_20260421064037.jsx
+++
@@ -1,7 +0,0 @@
-import Books from "./pages/Books";
-
-function App() {
-  return <Books />;
-}
-
-export default App;
--- .history/src/App_20260421193618.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <h1 className="text-4xl text-green-700 font-bold">
-      Tailwind aktif 🚀
-    </h1>
-  );
-}
-
-export default App;
